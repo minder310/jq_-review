9e55882d0fa0960bb7b8e215377f33aeaadab9e2
pierre.K <minder309@gmail.com>
2023-03-29 10:13:46 +0800

    測試show,hide,toggle

--- js/all.js
+++ js/all.js
@@ -1,10 +1,14 @@
 // 這個是起手式，可以用來寫JQ，裡面才可以寫jq
 $(document).ready(function () {
+    // 當我點選class button時，會執行function裡面，的程式碼。
     $('.button').click(function (e) { 
         // 這邊前面可以寫class裡面的哪個元件。顯示或是隱藏。
-        $('.title h1').hide(); 
+        // $('.title #test').hide();
+        // 綁定特定物件，後面teggle()判斷現在是顯示或是隱藏，隱藏他就顯示，顯示他就隱藏。
+        $('.title #test').toggle(); 
     });
     $('.button2').click(function (e) { 
-        $('.title h1').show();
+        // 綁定特定物件，顯示物件。
+        $('.title #test').show();
     });
 });

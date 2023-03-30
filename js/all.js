6192383d2a725c520a7c8281a197faf61c5d29ea
pierre.K <minder309@gmail.com>
2023-03-30 08:50:12 +0800

    複習css絕對定位相對定位。

--- js/all.js
+++ js/all.js
@@ -7,11 +7,13 @@ $(document).ready(function () {
         // 綁定特定物件，後面teggle()判斷現在是顯示或是隱藏，隱藏他就顯示，顯示他就隱藏。
         $('.title #test').toggle(); 
     });
+    // 綁定留言板顯示按鈕，當要留言時才
     $('.button2').click(function (e) { 
         // 綁定特定物件，顯示物件。
-        $('.title #test').show();
+        // slideDown是指慢慢顯示用三秒。
+        $('.text').slideToggle(3000);
     });
-    // 當滑鼠移到class hover。
+    // 當滑鼠hover，滑入滑出時動作。。
     $('.hover').hover(function () {
         // 滑鼠滑到時顯示。
             $('.title #test').show();
@@ -21,4 +23,16 @@ $(document).ready(function () {
             $('.title #test').hide();
         }
     );
+    $('.fade-button').click(function (e) { 
+        // 自動偵測現在隱藏或顯示，當隱藏就顯示，當顯示舊隱藏。
+        $('.fade').fadeToggle();
+        // 漸漸顯示。
+        // $('綁定物件').fadeIn("秒數");
+        // 漸漸隱藏。
+        // $('綁定物件').fadeOut("秒數");
+    });
+    $('.toggle-button').click(function (e) { 
+        $('.toggle').toggleClass("active");
+        
+    });
 });
